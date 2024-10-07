c813cc6873843aa6ce9529a8dce39543cade5e0d
Shilpi2525 <shilpi.shaw@pyxeda.ai>
2024-10-07 21:46:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
 
+
+@st.cache_resource
+def load_sklearn_models(model_path):
+
+    with open(model_path, 'rb') as model_file:
+        final_model = pickle.load(model_file)
+
+    return final_model
+
+# load the model
+ADHD_model = load_sklearn_models("")
+
+#title of the web page
+st.title("ADHD Predicting WebApp")
